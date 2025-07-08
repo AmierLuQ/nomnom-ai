72ebef2f7047ffd6e455d11cf8c2d72302d03032
AmierLuQ <amier.luqman@gmail.com>
2025-07-09 03:45:43 +0800

    new ui

--- nomnom-frontend/src/pages/RegisterPage.jsx
+++ nomnom-frontend/src/pages/RegisterPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/RegisterPage.css";
+import "../styles/AuthPage.css";
 
 export default function RegisterPage() {
   const navigate = useNavigate();
