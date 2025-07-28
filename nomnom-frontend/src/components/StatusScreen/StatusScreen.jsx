d11aaf5e134c7e5718f0f397d42df4d0e0fd8c33
AmierLuQ <amier.luqman@gmail.com>
2025-07-28 16:18:09 +0800

    adjust statusscreen

--- nomnom-frontend/src/components/StatusScreen/StatusScreen.jsx
+++ nomnom-frontend/src/components/StatusScreen/StatusScreen.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const StatusScreen = ({ message, showLogo = true }) => (
     <div className="home-container">
         <div className="status-container">
-            {showLogo && <img src="/nomnom-ai logo.PNG" alt="NomNom AI Logo" className="status-logo" />}
+            {showLogo && <img src="../../assets/images/nomnom-ai logo.PNG" alt="NomNom AI Logo" className="status-logo" />}
             <p className="status-message">{message}</p>
         </div>
     </div>
